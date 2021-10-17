0cac015190c20469aeddd63c86783b21c2a83917
C.Meirelles <clara_meirelles@hotmail.com>
2021-10-17 20:55:17 -0300

    correção dos exercícios de node

--- 01-node-npm/exercicio2/src/exercicio2.js
+++ 01-node-npm/exercicio2/src/exercicio2.js
@@ -7,19 +7,18 @@ const numero1 = Number(process.argv[2])
 const operacaoInput = process.argv[3]
 const numero2 = Number(process.argv[4])
 let resultado
-
+console.log(operacaoInput)
 if (operacaoInput === "+" || operacaoInput === "add") {
     resultado = numero1 + numero2
 } else if (operacaoInput === "-" || operacaoInput === "sub") {
     resultado = numero1 - numero2
 } else if (operacaoInput === "*" || operacaoInput === "mult") {
     resultado = numero1 * numero2
-} else if (operacaoInput === "/" || operacaoInput === "div") {
+} else if (operacaoInput === ":" || operacaoInput === "div") {
     resultado = (numero1 / numero2)
 }
 if (isNaN(resultado)) {
-    resultado = red("digite apenas números ou operadores válidos (+, -, *, /)")
+    resultado = red("digite apenas números ou operadores válidos (+, -, *, :)")
 } else {
     console.log("O resultado é", green(resultado))
 }
-// Divisão não funciona com o operador
